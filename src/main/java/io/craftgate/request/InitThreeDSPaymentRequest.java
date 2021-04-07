f171cbbe618d9397ad80ba6e4fb320250be6d493
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2021-04-07 23:39:40 +0300

    fixes compilation warnings (#22)

--- src/main/java/io/craftgate/request/InitThreeDSPaymentRequest.java
+++ src/main/java/io/craftgate/request/InitThreeDSPaymentRequest.java
@@ -1,10 +1,12 @@
 package io.craftgate.request;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@EqualsAndHashCode(callSuper = true)
 public class InitThreeDSPaymentRequest extends CreatePaymentRequest {
 
     private String callbackUrl;
