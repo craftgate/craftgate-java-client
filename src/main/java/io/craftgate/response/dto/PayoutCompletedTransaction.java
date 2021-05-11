5efcf8f894ef3e3d577a53b435e3e3863feb428b
Alican Akkuş <alican.akkus94@gmail.com>
2021-05-11 15:37:20 +0300

    adds payout id to payout completed transactions response (#36)

--- src/main/java/io/craftgate/response/dto/PayoutCompletedTransaction.java
+++ src/main/java/io/craftgate/response/dto/PayoutCompletedTransaction.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 @Data
 public class PayoutCompletedTransaction {
 
+    private Long payoutId;
     private Long transactionId;
     private String transactionType;
     private BigDecimal payoutAmount;
