ed5cb8ebf37efa7522e4bd5c714b7a0786450f9e
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2021-06-18 11:48:39 +0300

    adds payout fields to bounced rows resource (#41)

--- src/main/java/io/craftgate/response/dto/PayoutBouncedTransaction.java
+++ src/main/java/io/craftgate/response/dto/PayoutBouncedTransaction.java
@@ -2,12 +2,20 @@ package io.craftgate.response.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Data
 public class PayoutBouncedTransaction {
 
     private Long id;
     private String iban;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private Long payoutId;
+    private BigDecimal payoutAmount;
     private String contactName;
     private String contactSurname;
     private String legalCompanyTitle;
+    private String rowDescription;
 }
