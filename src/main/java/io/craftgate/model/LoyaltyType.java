9e962493320ab85f9d7fc7068d716dbedf3afa94
Onur Polattimur <onur.polattimur@gmail.com>
2021-12-08 09:58:56 +0300

    Add YKB loyalty types (#66)

--- src/main/java/io/craftgate/model/LoyaltyType.java
+++ src/main/java/io/craftgate/model/LoyaltyType.java
@@ -1,5 +1,10 @@
 package io.craftgate.model;
 
 public enum LoyaltyType {
-    REWARD_MONEY
+    REWARD_MONEY,
+    ADDITIONAL_INSTALLMENT,
+    POSTPONING_INSTALLMENT,
+    EXTRA_POINTS,
+    GAINING_MINUTES,
+    POSTPONING_STATEMENT
 }
