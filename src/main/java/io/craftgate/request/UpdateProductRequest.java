e3720b511e08c793f2b576a56482f56ec3c74a0e
Alican Akkuş <alican.akkus94@gmail.com>
2022-04-15 09:15:08 +0300

    adds paybylink adapter (#80)

---
+++ src/main/java/io/craftgate/request/UpdateProductRequest.java
@@ -0,0 +1,23 @@
+package io.craftgate.request;
+
+import io.craftgate.model.Currency;
+import io.craftgate.model.Status;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Data
+@Builder
+public class UpdateProductRequest {
+
+    private String name;
+    private String channel;
+    private Status status;
+    private Integer stock;
+    private BigDecimal price;
+    private Currency currency;
+    private String description;
+    private Set<Integer> enabledInstallments;
+}
