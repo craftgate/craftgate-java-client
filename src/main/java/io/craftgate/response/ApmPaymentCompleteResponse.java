1be1412047ee071de2ca7d04e60a44fb90878303
Alican Akkuş <alican.akkus@craftgate.io>
2022-12-01 12:03:17 +0300

    adds error detail to apm payment responses (#103)

--- src/main/java/io/craftgate/response/ApmPaymentCompleteResponse.java
+++ src/main/java/io/craftgate/response/ApmPaymentCompleteResponse.java
@@ -1,6 +1,7 @@
 package io.craftgate.response;
 
 import io.craftgate.model.PaymentStatus;
+import io.craftgate.response.dto.PaymentError;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,5 @@ public class ApmPaymentCompleteResponse {
 
     private Long paymentId;
     private PaymentStatus paymentStatus;
+    private PaymentError paymentError;
 }
--- src/main/java/io/craftgate/response/ApmPaymentInitResponse.java
+++ src/main/java/io/craftgate/response/ApmPaymentInitResponse.java
@@ -2,6 +2,7 @@ package io.craftgate.response;
 
 import io.craftgate.model.ApmAdditionalAction;
 import io.craftgate.model.PaymentStatus;
+import io.craftgate.response.dto.PaymentError;
 import lombok.Data;
 
 @Data
@@ -11,4 +12,5 @@ public class ApmPaymentInitResponse {
     private String redirectUrl;
     private PaymentStatus paymentStatus;
     private ApmAdditionalAction additionalAction;
+    private PaymentError paymentError;
 }
