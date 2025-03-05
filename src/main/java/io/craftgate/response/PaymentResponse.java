ebd681f98a1d979148e4683af87997e395fd2908
Serhat Tunca <35580464+tuncaserhat@users.noreply.github.com>
2025-03-05 15:57:45 +0300

    Add fraudId and fraudAction payment reporting retrieve (#234)

--- src/main/java/io/craftgate/response/PaymentResponse.java
+++ src/main/java/io/craftgate/response/PaymentResponse.java
@@ -1,6 +1,5 @@
 package io.craftgate.response;
 
-import io.craftgate.model.FraudAction;
 import io.craftgate.response.common.BasePaymentResponse;
 import io.craftgate.response.dto.PaymentTransaction;
 import lombok.Data;
@@ -15,8 +14,6 @@ public class PaymentResponse extends BasePaymentResponse {
 
     private String cardUserKey;
     private String cardToken;
-    private Long fraudId;
-    private FraudAction fraudAction;
     private Map<String,Object> additionalData;
     private List<PaymentTransaction> paymentTransactions;
 }
