f822596f5ca3f9a0c794c879c0d2e81f651ae2a6
Alican Akkus <aakkus@MacBook-Pro-2.local>
2025-08-07 11:37:48 +0300

    Add papel as a new apm type

--- src/main/java/io/craftgate/model/ApmAdditionalAction.java
+++ src/main/java/io/craftgate/model/ApmAdditionalAction.java
@@ -1,11 +1,12 @@
 package io.craftgate.model;
 
 public enum ApmAdditionalAction {
-    REDIRECT_TO_URL,
 
+    REDIRECT_TO_URL,
     OTP_REQUIRED,
     SHOW_HTML_CONTENT,
     WAIT_FOR_WEBHOOK,
     APPROVAL_REQUIRED,
+    SHOW_QR_CODE,
     NONE
 }
