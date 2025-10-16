07bbdea6dba41cf7ca3517ab9ff0cb35f03aee93
Burak Yıldız <burak.yildiz@craftgate.io>
2025-10-16 14:19:07 +0300

    Update allowDeleteStoredCard field to InitCheckoutPaymentRequest

--- src/main/java/io/craftgate/request/InitCheckoutPaymentRequest.java
+++ src/main/java/io/craftgate/request/InitCheckoutPaymentRequest.java
@@ -41,7 +41,7 @@ public class InitCheckoutPaymentRequest {
     protected Long buyerMemberId;
     protected List<Integer> enabledInstallments;
     protected boolean alwaysStoreCardAfterPayment;
-    protected boolean enabledDeleteCard;
+    protected boolean allowDeleteStoredCard;
     protected boolean allowOnlyStoredCards;
     protected boolean allowOnlyCreditCard;
     protected boolean allowInstallmentOnlyCommercialCards;
