e5bb5dc8fcd2110a3da4169db0fa6cfa340c47e5
furkanozmen <furkan.ozmen@craftgate.io>
2025-10-24 17:26:37 +0300

    Remove unused report periods and types from enums

--- src/main/java/io/craftgate/model/ReportPeriod.java
+++ src/main/java/io/craftgate/model/ReportPeriod.java
@@ -1,6 +1,5 @@
 package io.craftgate.model;
 
 public enum ReportPeriod {
-    DAILY,
     INSTANT
 }
--- src/main/java/io/craftgate/model/ReportType.java
+++ src/main/java/io/craftgate/model/ReportType.java
@@ -2,7 +2,5 @@ package io.craftgate.model;
 
 public enum ReportType {
     TRANSACTION,
-    SETTLEMENT,
     PAYMENT,
-    SUB_MERCHANT_MEMBER_WALLET_TX
 }
