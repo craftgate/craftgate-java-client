8eb977940222db71bff5636bdd15cf5d7c2df9cc
Mustafa OZCAN <wustafa.ozcan@gmail.com>
2026-01-23 15:59:06 +0300

    Add `guestCheckout` field to `InitCheckoutPaymentRequest` (#287)
    * Add `guestCheckout` field to `InitCheckoutPaymentRequest`
    
    * Optimize imports in `PaymentSample.java` by using wildcard imports.

--- src/test/java/io/craftgate/sample/PaymentSample.java
+++ src/test/java/io/craftgate/sample/PaymentSample.java
@@ -801,6 +801,7 @@ public class PaymentSample {
                 .conversationId("456d1297-908e-4bd6-a13b-4be31a6e47d5")
                 .paymentGroup(PaymentGroup.LISTING_OR_SUBSCRIPTION)
                 .paymentPhase(PaymentPhase.AUTH)
+                .guestCheckout(false)
                 .items(items)
                 .build();
 
