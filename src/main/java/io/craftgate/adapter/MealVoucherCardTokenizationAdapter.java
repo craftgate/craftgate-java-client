179133350da54517d6ec2848377454592089303e
Serhat Tunca <serhat.tunca@craftgate.io>
2026-04-21 11:03:03 +0300

    Add RequestContext support for card tokenization init requests

--- src/main/java/io/craftgate/adapter/MealVoucherCardTokenizationAdapter.java
+++ src/main/java/io/craftgate/adapter/MealVoucherCardTokenizationAdapter.java
@@ -2,6 +2,7 @@ package io.craftgate.adapter;
 
 import io.craftgate.net.HttpClient;
 import io.craftgate.request.MealVoucherCardTokenizationInitRequest;
+import io.craftgate.request.common.RequestContext;
 import io.craftgate.request.common.RequestOptions;
 import io.craftgate.response.MealVoucherCardTokenizationInitResponse;
 
@@ -12,8 +13,12 @@ public class MealVoucherCardTokenizationAdapter extends BaseAdapter {
     }
 
     public MealVoucherCardTokenizationInitResponse cardTokenizationInit(MealVoucherCardTokenizationInitRequest mealVoucherCardTokenizationInitRequest) {
+        return cardTokenizationInit(mealVoucherCardTokenizationInitRequest, null);
+    }
+
+    public MealVoucherCardTokenizationInitResponse cardTokenizationInit(MealVoucherCardTokenizationInitRequest mealVoucherCardTokenizationInitRequest, RequestContext requestContext) {
         String path = "/payment/v1/meal-voucher/card-tokenizations/init";
-        return HttpClient.post(requestOptions.getBaseUrl() + path, createHeaders(mealVoucherCardTokenizationInitRequest, path, requestOptions),
+        return HttpClient.post(requestOptions.getBaseUrl() + path, createHeaders(mealVoucherCardTokenizationInitRequest, path, requestContext),
                 mealVoucherCardTokenizationInitRequest, MealVoucherCardTokenizationInitResponse.class);
     }
 }
