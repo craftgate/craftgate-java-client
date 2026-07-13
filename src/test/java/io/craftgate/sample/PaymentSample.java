c0b153e71cbdc43b4cd802e1f761f6e80fa4cfb2
Serhat Tunca <35580464+tuncaserhat@users.noreply.github.com>
2026-07-13 09:55:59 +0300

    Add `enabledInstallments` field to `InitMultiPaymentRequest` (#319)
    * Add `enabledInstallments` field to `InitMultiPaymentRequest`
    
    * Add `enabledInstallments` field to `InitMultiPaymentRequest`
    
    ---------
    
    Co-authored-by: Mustafa OZCAN <wustafa.ozcan@gmail.com>

--- src/test/java/io/craftgate/sample/PaymentSample.java
+++ src/test/java/io/craftgate/sample/PaymentSample.java
@@ -2313,7 +2313,6 @@ public class PaymentSample {
                 .conversationId("456d1297-908e-4bd6-a13b-4be31a6e47d5")
                 .externalId("external_id-123456789")
                 .paymentGroup(PaymentGroup.LISTING_OR_SUBSCRIPTION)
-                .paymentPhase(PaymentPhase.AUTH)
                 .items(items)
                 .build();
 
